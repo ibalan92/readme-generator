a9d9fc80ded1a8da676c11b4b2a862fe238ae642
ibalan92 <ibalan92@gmail.com>
2024-01-18 14:16:19 +0000

    Added questions in the array

--- index.js
+++ index.js
@@ -5,6 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your Github username?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project name?",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please write a description of your project",
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: "What kind of license should your project have?",
+        choices: ['MIT License', 'Apache License 2.0', 'Boost Software License 1.0'],
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "What command should be run to install dependencies?",
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What command should be run to run tests?",
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'info',
+        message: "What does the user need to know about using the repo?",
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "What does the user need to know about contributing to the the repo?",
+    },
 
 ];
 
